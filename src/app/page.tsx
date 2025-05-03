0c51a49c8e69bdcaa2be798c060e2e1f211e1c06
Yongbi21 <ajromblon21@yahoo.com>
2025-05-03 08:56:19 +0800

    add-header+intro-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,89 @@
-import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+    <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-4 sm:p-20 bg-[#000]
+    font-[family-name:var(--font-poppins)] relative flex-col justify-center">
+      <header className="row-start-1 flex gap-[18px] flex-wrap item-start text-white">
+        <a
+            className="flex items-center gap-2  font-semibold text-2xl"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        >
+          Plan →
+        </a>
+        <a
+            className="flex items-center gap-2  font-semibold text-2xl"
             target="_blank"
             rel="noopener noreferrer"
+        >
+          Develop →
+        </a>
+        <a
+            className="flex items-center gap-2  font-semibold text-2xl"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          Launch
+        </a>
+
+      </header>
+      <main className="flex row-start-2 sm:items-start mt-10 justify-center">
+        <Card className="bg-[#1a1a1a] text-white border-none ">
+          <CardContent className="flex flex-col gap-4 my-2 mx-2">
+          <h1 className="text-2xl font-light text-white">
+            Hello there! I&#39;m <span className="font-semibold">Alvin John Romblon</span>
+           </h1>
+
+        <p className="text-2xl text-white font-light">
+          I turn ideas into refined, usable websites.
+        </p>
+
+        <p className="text-md text-white font-light max-w-xl">
+          I&#39;m a front-end developer crafting user-focused interfaces that simplify daily tasks through thoughtful, intentional design.
+        </p>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row mt-3">
+          <a
+              className="rounded-md border border-transparent transition-colors flex items-center justify-center
+                 bg-[#2AC6A4] text-white gap-2 font-medium text-md sm:text-base h-10 sm:h-11 px-4 sm:px-5 sm:w-auto"
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
           >
-            Read our docs
+            Get in touch
           </a>
         </div>
+            </CardContent>
+        </Card>
+
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+
+
+
     </div>
   );
 }
+
+
+// <div className="flex gap-4 items-center flex-col sm:flex-row">
+//   <a
+//       className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center
+//              bg-[#2AC6A4] text-white gap-2  font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+//       href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//   >
+//     Deploy now
+//   </a>
+// </div>
